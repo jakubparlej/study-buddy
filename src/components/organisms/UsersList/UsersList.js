b0dd35db342b4ea93c81b9fba3301aa3f05df9e1
Jakub Parlej <jakubparlej@gmail.com>
2022-01-04 14:08:23 +0100

    Components-lifecycle final

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,14 +1,45 @@
-import React from "react";
-import { users } from "../../../data/users.js";
+import React, { useState, useEffect } from "react";
+import { users as usersData } from "../../../data/users.js";
 import UsersListItem from "../../molecules/UsersListItem/UsersListItem.js";
 import { Wrapper, StyledList } from "./UsersList.styles";
 
+const mockAPI = (success) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (usersData) {
+        resolve([...usersData]);
+      } else {
+        reject({ message: "Error" });
+      }
+    }, 2000);
+  });
+};
+
 const UsersList = () => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoadingState] = useState(true);
+
+  useEffect(() => {
+    setLoadingState(true);
+    mockAPI()
+      .then((data) => {
+        setLoadingState(false);
+        setUsers(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const deleteUser = (name) => {
+    const filteredUsers = users.filter((user) => user.name !== name);
+    setUsers(filteredUsers);
+  };
+
   return (
     <Wrapper>
+      <h1>{isLoading ? "Loading..." : "Users list"}</h1>
       <StyledList>
-        {users.map((userData, index) => (
-          <UsersListItem key={index} userData={userData} />
+        {users.map((userData, i) => (
+          <UsersListItem deleteUser={deleteUser} key={i} userData={userData} />
         ))}
       </StyledList>
     </Wrapper>
